2844106494ec160d4a414d974f5635dea2f9b1bf
arkadievvladimir <arkadiev.vladimir@gmail.com>
2020-10-01 00:34:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const symbolCount = expr.length/10;
+    let message = '';
+    let i = 1;
+
+    while (i <= symbolCount) {
+      if (expr.slice(0, 10) === '**********') {
+        message = `${message} `;
+      } else{ 
+        message = `${message}${MORSE_TABLE[binToMorse(expr.slice(0, 10))]}`;
+      }
+      expr = expr.slice(10) 
+      i++;
+    }
+
+    return message
 }
+
+function binToMorse(bin) {
+  let resultMorse = '';
+  let i = 1;
+
+  while (i <= 5) {
+    if (bin.slice(-2) === '10') resultMorse = `.${resultMorse}`;
+    if (bin.slice(-2) === '11') resultMorse = `-${resultMorse}`;
+    if (bin.slice(-2) === '00') return resultMorse;
+    bin = bin.slice(0, -2);
+    i++;
+  }
+
+  return resultMorse;
+}
+
+
+
 
 module.exports = {
     decode
